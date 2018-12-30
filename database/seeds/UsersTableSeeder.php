180c805b46a8a62b5cb847ae7a5de83bff50f8f5
Jyoti Duwal <duwaljyoti16@gmail.com>
2018-12-30 15:35:02 +0545

    pma and migrations working well.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,9 +1,20 @@
 <?php
 
+use Illuminate\Database\Seeder;
 
-namespace App\Database;
-
-class UsersTableSeeder
+class UsersTableSeeder extends Seeder
 {
-
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \Illuminate\Support\Facades\DB::table('users')->insert([
+            'name' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+    }
 }
